7b664ebc1ceb84c646843d02e2351d6fb42230a6
Lucas V. Araujo <lucas.vieira.ar@disroot.org>
2020-02-01 20:24:58 -0300

    solved: set2/challenge9

---
+++ set2/challenge9.pl
@@ -0,0 +1,51 @@
+package PKCS7;
+use utf8;
+use strict;
+use warnings;
+use Exporter qw( import );
+
+our @EXPORT_OK = qw ( pkcs7_pad pkcs7_padded pkcs7_unpad );
+
+sub pkcs7_pad {
+    my ($data, $size) = @_;
+    my $pad_char = $size - length($data) % $size;
+    $data . chr($pad_char)x$pad_char;
+}
+
+sub pkcs7_padded
+{
+    my $data  = shift;
+    my $last  = ord(substr $data, -1, 1);
+    my $block = substr $data, -$last;
+    for my $byte (split //, $block)
+    {
+        return 0 if ord($byte) != $last;
+    }
+    1;
+}
+
+sub pkcs7_unpad
+{
+    my $data = shift;
+    return $data unless pkcs7_padded($data);
+    my $last = ord(substr $data, -1, 1);
+    substr $data, 0, length($data) - $last;
+}
+
+sub test
+{
+    my $text     = "YELLOW SUBMARINE";
+    my $padded   = pkcs7_pad($text, 20);
+    my $unpadded = pkcs7_unpad($text);
+    if (pkcs7_padded($padded))
+    {
+        print "PADDED - OK\n";
+    }
+
+    if ($text eq $unpadded)
+    {
+        print "UNPADDED - OK\n";
+    }
+}
+
+test unless caller;
