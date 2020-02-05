5055606f6b1997f18eb77a0e752be12df7b82f89
Lucas V. Araujo <lucas.vieira.ar@disroot.org>
2020-02-05 18:01:26 -0300

    cleaned up

---
+++ challenges/challenge17.pl
@@ -0,0 +1,34 @@
+use utf8;
+use strict;
+use warnings;
+use MIME::Base64;
+require "../set2/challenge10.pl";
+
+my @input_strings = ( "MDAwMDAwTm93IHRoYXQgdGhlIHBhcnR5IGlzIGp1bXBpbmc=",
+"MDAwMDAxV2l0aCB0aGUgYmFzcyBraWNrZWQgaW4gYW5kIHRoZSBWZWdhJ3MgYXJlIHB1bXBpbic=",
+"MDAwMDAyUXVpY2sgdG8gdGhlIHBvaW50LCB0byB0aGUgcG9pbnQsIG5vIGZha2luZw==",
+"MDAwMDAzQ29va2luZyBNQydzIGxpa2UgYSBwb3VuZCBvZiBiYWNvbg==",
+"MDAwMDA0QnVybmluZyAnZW0sIGlmIHlvdSBhaW4ndCBxdWljayBhbmQgbmltYmxl",
+"MDAwMDA1SSBnbyBjcmF6eSB3aGVuIEkgaGVhciBhIGN5bWJhbA==",
+"MDAwMDA2QW5kIGEgaGlnaCBoYXQgd2l0aCBhIHNvdXBlZCB1cCB0ZW1wbw==",
+"MDAwMDA3SSdtIG9uIGEgcm9sbCwgaXQncyB0aW1lIHRvIGdvIHNvbG8=",
+"MDAwMDA4b2xsaW4nIGluIG15IGZpdmUgcG9pbnQgb2g=",
+"MDAwMDA5aXRoIG15IHJhZy10b3AgZG93biBzbyBteSBoYWlyIGNhbiBibG93" );
+
+my $aes_key;
+
+sub encrypt_random
+{
+    my $string = decode_base64 $input_strings[rand @input_strings];
+    $aes_key   = join '', map {chr rand 256} 1 .. 16 unless $aes_key;
+    my $iv     = join '', map {chr rand 256} 1 .. 16;
+    (AES_CBC::encrypt_data($string, $aes_key, $iv), $iv);
+}
+
+sub test
+{
+    my ($cipher, $iv) = encrypt_random;
+    print "$cipher\n";
+}
+
+test unless caller;
