c0cf5f302322334d4f5ca680a063e39b274c9c15
Lucas V. Araujo <lucas.vieira.ar@disroot.org>
2020-02-06 17:52:44 -0300

    (not) fixed set3/challenge17

--- challenges/challenge17.pl
+++ challenges/challenge17.pl
@@ -2,8 +2,8 @@ use utf8;
 use strict;
 use warnings;
 use MIME::Base64;
+require "./challenge9.pl";
 require "./challenge10.pl";
-require "./challenge15.pl";
 
 my @input_strings = ( "MDAwMDAwTm93IHRoYXQgdGhlIHBhcnR5IGlzIGp1bXBpbmc=",
 "MDAwMDAxV2l0aCB0aGUgYmFzcyBraWNrZWQgaW4gYW5kIHRoZSBWZWdhJ3MgYXJlIHB1bXBpbic=",
@@ -17,12 +17,13 @@ my @input_strings = ( "MDAwMDAwTm93IHRoYXQgdGhlIHBhcnR5IGlzIGp1bXBpbmc=",
 "MDAwMDA5aXRoIG15IHJhZy10b3AgZG93biBzbyBteSBoYWlyIGNhbiBibG93" );
 
 my $aes_key;
+my $aes_iv;
 
 sub encrypt_random
 {
-    my $string = decode_base64 $input_strings[rand @input_strings];
+    my $string = "Lucas Vieira Araujo, aka LvMalware - 2020";#decode_base64 $input_strings[rand @input_strings];
     $aes_key   = join '', map {chr rand 256} 1 .. 16 unless $aes_key;
-    my $aes_iv = join '', map {chr rand 256} 1 .. 16;
+    $aes_iv    = join '', map {chr rand 256} 1 .. 16 unless $aes_iv;
     (AES_CBC::encrypt_data($string, $aes_key, $aes_iv), $aes_iv);
 }
 
@@ -30,13 +31,13 @@ sub decrypt_check
 {
     my ($encrypted, $iv) = @_;
     my $decrypted = AES_CBC::decrypt_data($encrypted, $aes_key, $iv, 0);
-    PKCS7_V::padded($decrypted);
+    PKCS7::pkcs7_padded($decrypted);
 }
 
 
 sub padding_oracle_block
 {
-    #This works sometimes and sometimes give a fucking weird result
+    #This works sometimes and sometimes we get a fucking weird result
     #Tell me if you see what is wrong :)
     my ($block, $iv) = @_;
     my $decrypted    = "";
@@ -47,7 +48,7 @@ sub padding_oracle_block
         my $spoof_bytes  = join '', map {
             chr(ord($_) ^ $padding_size)
             } split //, $decrypted;
-        
+
         for my $byte (0 .. 255)
         {
             if (decrypt_check($block, $tmp_iv . chr($byte) . $spoof_bytes))
@@ -62,8 +63,12 @@ sub padding_oracle_block
         my $dec_byte = ord substr($decrypted, $i, 1);
         my $iv_byte  = ord substr($iv, $i, 1);
         substr($decrypted, $i, 1) = chr ($dec_byte ^ $iv_byte);
-    }
+    };
     $decrypted;
+}
+
+sub split_blocks {
+    map { substr $_[0], $_ * $_[1], $_[1] } 0 .. length($_[0])/$_[1] - 1
 }
 
 sub padding_oracle_attack
@@ -71,19 +76,20 @@ sub padding_oracle_attack
     my ($encrypted, $iv) = @_;
     my $block_size       = 16; #we already know the block size
     my $decrypted        = "";
-    for my $i (0 .. int(length($encrypted) / $block_size) - 1)
+    my @blocks           = ( $iv );
+    push @blocks, split_blocks($encrypted, $block_size);
+    for (my $i = 1; $i < scalar @blocks; $i++)
     {
-        my $block = substr $encrypted, $i * $block_size, $block_size;
-        $decrypted .= padding_oracle_block($block, $iv);
-        $iv = $block;
+        $decrypted .= padding_oracle_block($blocks[$i], $blocks[$i-1]);
     }
-    $decrypted;
+    PKCS7::pkcs7_unpad $decrypted;
 }
 
 sub test
 {
     #still it makes the text a little weird sometimes ... 
-    print "|" . padding_oracle_attack(encrypt_random) . "|\n";
+    my $decrypted  = padding_oracle_attack(encrypt_random);
+    print "$decrypted\n";
 }
 
 test unless caller;
