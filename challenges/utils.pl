9348eb800839731b45277f0302b9f9e046ba2104
Lucas V. Araujo <lucas.vieira.ar@disroot.org>
2020-02-13 17:59:26 -0300

    changed utils.pl

--- challenges/utils.pl
+++ challenges/utils.pl
@@ -8,3 +8,20 @@ our @EXPORT_OK = qw ( random_bytes );
 
 #I use it a lot, so...
 sub random_bytes{ join '', map { chr rand 256 } 1 .. $_[0] }
+
+sub choose_key
+{
+    my $words = "/usr/share/dict/words";
+    my $count = `wc -l $words | cut -d " " -f1`;
+    my $index = int rand $count;
+    my $file;
+    open $file, "< :encoding(UTF-8)", $words;
+    while ($index)
+    {
+        <$file>;
+        $index --;
+    }
+    $secret_key = <$file>;
+    chomp $secret_key;
+    close $file;
+}
