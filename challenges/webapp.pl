303d059e8c3fc72516097fc8dca2d28274886f7a
Lucas V. Araujo <lucas.vieira.ar@disroot.org>
2020-02-17 21:25:07 -0300

    updated webapp.pl

--- challenges/webapp.pl
+++ challenges/webapp.pl
@@ -2,6 +2,7 @@ use strict;
 use warnings;
 use Mojolicious::Lite;
 use List::MoreUtils 'zip';
+use Time::HiRes qw(usleep); #for a more precise sleep function
 require "./sha1.pl";
 use lib ".";
 use HMAC;
@@ -20,12 +21,12 @@ sub sha1_hmac
 sub insecure_equals
 {
     my ($str1, $str2) = @_;
-    for (my $i = 0; $i < length($str1); $i +=2)
+    for (my $i = 0; $i < length($str1); $i ++)
     {
-        my $byte1 = hex substr($str1, $i, 2);
-        my $byte2 = hex substr($str2, $i, 2);
+        my $byte1 = ord substr($str1, $i, 1);
+        my $byte2 = ord substr($str2, $i, 1);
         return 0 if ($byte1 != $byte2);
-        sleep 0.005;
+        usleep(50000); #1 millisecond = 1000 microseconds
     }
     return 1;
 }
